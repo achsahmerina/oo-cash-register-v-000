b4f57113dde8609cdb4b187ea03e3f0d84186d04
Achsah Abraham <achsahmp@gmail.com>
2018-09-29 22:39:05 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,74 +1,34 @@
 class CashRegister
-  attr_accessor :total, :discount
-
-  def initialize(discount = 0)
+  attr_accessor :total, :discount, :quantity, :price
+   def initialize(discount = 0)
     @total = 0
     @discount = discount
     @price = price
-    @transaction = []
+    @quantity = quantity
+    @items = []
+    @transactions = []
   end
-
-  def add_item(title, price, increases = 1)
-    item_name = {}
-    item_name[:name] = item
-    item_name[:price] = price
-    item_name[:increases] = increases
-
-    @transaction << item_name
+   def add_item(title, price, quantity = 1)
     @total += price * quantity
+    @transactions << price
+    i = quantity
+    until i == 0 do
+      @items << title
+      i -= 1
+    end
+  end
+   def apply_discount
+    if @discount == 0
+      "There is no discount to apply."
+    else
+      self.total -= (0.01 * @discount * @total).to_i
+      "After the discount, the total comes to $#{self.total}."
+    end
+  end
+   def items
+    @items
+  end
+   def void_last_transaction
+    self.total = @total - @transactions.pop
   end
 end
-# def apply_discount
-  # if @discount == 0
-  #   "There is no discount to apply."
-  # else
-  #   self.total -= (0.01 * @discount * @total).to_i
-  #   "After the discount, the total comes to $#{self.total}."
-#  end
- #end
-
-#def items
-#  @items
-#end
-
-
-
-#class CashRegister
-  #attr_accessor :total, :discount, :title, :previous_total, :items, :void_last_transaction
-
-#  def initialize(total = 0, discount = 20)
-  #  @total = total
-  #  @discount = discount
-    #@title = []
-  #  @increase = increase
-
-#  end
-
-#  def add_item(title, price, increase = 1)
-#    @total += price * increase
-  #  @transactions << price
-  #  i = increase
-  #  until i == 0 do
-  #  @items << title
-  #  i -= 1
-  #end
-  #end
-
-#  def apply_discount
-#   if @discount == 0
-#      "There is no discount to apply."
-#    else
-#      self.total -= (0.01 * @discount * @total).to_i
-  #    "After the discount, the total comes to $#{self.total}."
-  #  end
-  #end
-
-#  def items
-#    items = {}
-#    @items
-#  end
-
-#  def void_last_transaction
-#    self.total = self.total - self.transaction.pop
-#  end
-#end
