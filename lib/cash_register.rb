48d3c9f8010db60cc9f11fb2fdc1765ea43a788f
Achsah Abraham <achsahmp@gmail.com>
2018-09-29 22:33:46 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -24,7 +24,7 @@ end
    if @discount == 0
      "There is no discount to apply."
    else
-     self.total -= ( (0.01 * @discount * @total).to_i
+     self.total -= (0.01 * @discount * @total).to_i
      "After the discount, the total comes to $#{self.total}."
   end
  end
